3e90cc5dbb6d2276cadbed72cea0940e3f291341
LeiderCortes <cortes.leider@correounivalle.edu.co>
2023-11-29 18:31:44 -0500

    .

--- Taller3/punto1/StoogeSort/StoogeSort.py
+++ Taller3/punto1/StoogeSort/StoogeSort.py
@@ -11,32 +11,27 @@ def stoogesort(arr, s, e):
         arr[e] = t  # En la posicion e se guarda lo que tenia la variable temporal t.
 
 
-    if e - s + 1 > 2:  #  Si el array tiene mas de dos elementos
-        t = int((e - s + 1) / 3)
+    if e - s + 1 > 2:  #  Verifica si el array tiene mas de dos elementos
+        t = int((e - s + 1) / 3)  # Se le asigna un valor a t, el cual es el valor de 1/3 del segmento
 
-        # Recursively sort first 2/3 elements
-        stoogesort(arr, s, (e-t))
-
-        # Recursively sort last 2/3 elements
-        stoogesort(arr, s + t, e)
-
-        # Recursively sort first 2/3 elements
-        # again to confirm
-        stoogesort(arr, s, (e-t))
+        stoogesort(arr, s, (e-t))  # Recursion para ordenar los primeros 2/3 de segmento
+        stoogesort(arr, s + t, e)  # Recursion para ordenar los ultimos 2/3 de segmento
+        stoogesort(arr, s, (e-t))  # Repetimos la recursion inicial para organizar de nuevo los primeros 2/3, es necesario porque con el paso
+                                   # anterior, pudo haber cambiado el segundo sector.
 
 
 
-arr = []
+arr = []  # definicion del arreglo
 # n = 0 # Tamaño del arreglo
 # n = 10
 n = 100
 # n = 1000
-for i in range(n):
+for i in range(n):  # Darle valores aleatorios al arreglo.
     arr.append(random.randint(0, 100))
 
-stoogesort(arr, 0, n-1)
+stoogesort(arr, 0, n-1)  # Valores con los que va a trabajar el stoogesort
 
 for i in range(0, n):
-    print(arr[i], end = ' ')
+    print(arr[i], end=' ')  # Impresion de resultados
 
 
